237310c96ca29706e57632d591131e8abc3105c7
RecheEduardo <Eduardorechemartins2006@gmail.com>
2025-03-26 23:48:37 -0300

    big feat: memória da calculadora e limpeza do display, corrigindo tambem erro de somas consecutivas

--- src/telas/Calculadora2.java
+++ src/telas/Calculadora2.java
@@ -8,35 +8,42 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JTextField;
 
-public class Calculadora2 extends JFrame{
+public class Calculadora2  extends JFrame{
 	
 	private static final long serialVersionUID = 1L;
-	
+
 	private JTextField display = new JTextField();
 	
-	private JButton[] buttons = new JButton[16];	
-	private String[] labels = {"7", "8", "9", "+", "4", "5", "6", "-", "1", "2", "3", "*", ".", "0", "=", "/"};
-	private Font fonte1 = new Font("Arial", Font.BOLD, 26);
+	private JButton[] buttons = new JButton[20];	
+	private String[] labels = {"C","M1","M2","+/-","7", "8", "9", "+", "4", "5", "6", "-", "1", "2", "3", "*", ".", "0", "=", "/"};
+	private Font fonte1 = new Font("Arial", Font.BOLD, 20);
 	private Font fonte2 = new Font("Arial", Font.BOLD, 10);
 	
 	private String operador = "";
 	private String valorAcumulado = "";
+	private double memoria1;
+	private double memoria2;
+	private boolean apagaDisplay = false;
 	
 	public Calculadora2() {
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setTitle("Calculadora");
-		this.setBounds(500, 500, 225, 310);
+		this.setBounds(500, 500, 245, 370);
 		this.setLayout(null);
 		
 		this.display.setBounds   (  5, 5, 215, 40 );
 		this.display.setFont(fonte1);
 		
 		int i = 0;
-		for (int linha = 55; linha <= 220; linha+=55) {
+		for (int linha = 55; linha <= 275; linha+=55) {
 			for (int coluna = 5; coluna <=170; coluna+=55) {
 				this.buttons[i] = new JButton(this.labels[i]);
 				this.buttons[i].setBounds   ( coluna, linha, 50, 50);
-				this.buttons[i].setFont((this.buttons[i].getText() == "+/-")?fonte2:fonte1);	
+				this.buttons[i].setFont((
+					this.buttons[i].getText() == "+/-" || 
+					this.buttons[i].getText() == "M1" || 
+					this.buttons[i].getText() == "M2" ) ? fonte2:fonte1
+				);	
 				this.buttons[i].addActionListener(new ActionListener() {
 					public void actionPerformed(ActionEvent e) {
 						digito(((JButton) e.getSource()).getText());
@@ -49,16 +56,13 @@ public class Calculadora2 extends JFrame{
 		this.add(display);
 		this.setVisible(true);
 	}
-
-	public void calcular () {
-		if (( display.getText() != "") && (operador != "") && (valorAcumulado != "")) {
-			
-		}
-	}
 	
 	public void igual() {
 		if (( display.getText() != "") && (operador != "") && (valorAcumulado != "")) {
 			switch  (operador) {
+				case "+/-": {
+					display.setText(( Double.parseDouble(valorAcumulado) + Double.parseDouble(display.getText()))+"");
+				} break;
 				case "+": {
 					display.setText(( Double.parseDouble(valorAcumulado) + Double.parseDouble(display.getText()))+"");
 				} break;
@@ -81,20 +85,58 @@ public class Calculadora2 extends JFrame{
 		}
 	}
 	
-	public void calcular(String identificador) {
-		if (( valorAcumulado == "") && (operador == ""))	{
+	public void execOperador(String identificador) {
+		if (( valorAcumulado == "") && (operador == "")){
 			operador = identificador;
 			valorAcumulado = display.getText();
-			display.setText("");
+			// display.setText("");
+			apagaDisplay = true;
+		} else {
+			if(( valorAcumulado != "") && (operador != "")) {
+				igual();
+				operador = identificador;
+				valorAcumulado = display.getText();
+				apagaDisplay = true;
+			}
+		}
+	}
+	
+	public void zerarTudo() {
+		display.setText("");
+		valorAcumulado = "";
+		operador = "";
+	}
+	
+	public void addMemoria(String identificador) {
+		if(identificador == "M1") {
+			if(this.memoria1 != 0) {
+				display.setText(this.memoria1 + "");
+			}
+			else {
+				this.memoria1 = Double.parseDouble(display.getText());
+				zerarTudo();
+			}
+		}
+		if(identificador == "M2") {
+			if(this.memoria2 != 0) {
+				display.setText(this.memoria2 + "");
+			}
+			else {
+				this.memoria2 = Double.parseDouble(display.getText());
+				zerarTudo();
+			}
 		}
 	}
 	
 	public String digito( String identificador ) {
 		switch (identificador) {
-			case "+": { calcular(identificador); } break;
-			case "-": { calcular(identificador); } break;
-			case "*": { calcular(identificador); } break;
-			case "/": { calcular(identificador); } break;
+			case "C": { zerarTudo(); } break;
+			case "M1": {addMemoria(identificador);} break;
+			case "M2": {addMemoria(identificador);} break;
+			case "+": { execOperador(identificador); } break;
+			case "-": { execOperador(identificador); } break;
+			case "*": { execOperador(identificador); } break;
+			case "/": { execOperador(identificador); } break;
 			case "=": {  igual();  } break;
 			case ".": { 
 				if( ! display.getText().contains(".")) {
@@ -102,12 +144,14 @@ public class Calculadora2 extends JFrame{
 				}
 			} break;
 			default: {
+				if (apagaDisplay) {
+					display.setText("");
+					apagaDisplay = false;
+				}
 				display.setText(display.getText() + identificador);
 			}
 		}
 		
-		
 		return "";
 	}
-
 }
