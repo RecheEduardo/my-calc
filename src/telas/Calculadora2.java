5b1f3ea821afde98fc4b2ab28ec525189f807561
RecheEduardo <Eduardorechemartins2006@gmail.com>
2025-03-26 23:38:12 -0300

    feat: adicionando atributos para estrutura de repetição do Jframe

--- src/telas/Calculadora2.java
+++ src/telas/Calculadora2.java
@@ -1,5 +1,22 @@
 package telas;
 
-public class Calculadora2 {
+import java.awt.Font;
+
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+
+public class Calculadora2 extends JFrame{
+	
+	private static final long serialVersionUID = 1L;
+	
+	private JTextField display = new JTextField();
+	
+	private JButton[] buttons = new JButton[16];	
+	private String[] labels = {"7", "8", "9", "+", "4", "5", "6", "-", "1", "2", "3", "*", ".", "0", "=", "/"};
+	private Font fonte1 = new Font("Arial", Font.BOLD, 26);
+	private Font fonte2 = new Font("Arial", Font.BOLD, 10);
+	
+	private String operador = "";
+	private String valorAcumulado = ""; 
 
 }
